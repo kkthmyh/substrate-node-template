94a4723e62e240fcd0fe13c56714eba1cb26fb10
kkthmyh <2625038630@qq.com>
2022-01-07 21:23:23 +0800

    fix

--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -8,12 +8,12 @@ use frame_support::{assert_noop, assert_ok};
 fn create_works() {
     new_test_ext().execute_with(|| {
         let account_id: u64 = 1;
-        let kitty_id: u32 = 1;
+        let kitty_id: u32 = 0;
         // 创建Kitty
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id)));
         // 检查拥有者
         assert_eq!(Owner::<Test>::get(kitty_id), Some(account_id));
-        // // 检查创建成功事件
+        // 检查创建成功事件
         assert_has_event!(Event::<Test>::KittyCreate(account_id, kitty_id));
     });
 }
@@ -42,10 +42,10 @@ fn breed_works() {
     new_test_ext().execute_with(|| {
         let account_id: u64 = 1;
         // parent kitty
-        let kitty_id_1 = 1u32;
-        let kitty_id_2 = 2u32;
+        let kitty_id_1 = 0u32;
+        let kitty_id_2 = 1u32;
         // child kitty
-        let kitty_id_3: u32 = 3u32;
+        let kitty_id_3: u32 = 2u32;
         // 创建Kitty1
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id)));
         // 创建Kitty2
@@ -62,7 +62,7 @@ fn breed_failed_same_parent() {
     new_test_ext().execute_with(|| {
         let account_id: u64 = 1;
         // parent kitty
-        let kitty_id_1 = 1u32;
+        let kitty_id_1 = 0u32;
         // 创建Kitty1
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id)));
         // 繁殖时父母ID相同
@@ -75,8 +75,8 @@ fn breed_failed_invalid_kitty_index() {
     new_test_ext().execute_with(|| {
         let account_id: u64 = 1;
         // parent kitty
-        let kitty_id_1 = 1u32;
-        let kitty_id_2 = 2u32;
+        let kitty_id_1 = 0u32;
+        let kitty_id_2 = 1u32;
         // 繁殖时没有找到Id对应的Kitty
         assert_noop!(SubstrateKitties::breed(Origin::signed(account_id), kitty_id_1, kitty_id_2),Error::<Test>::InvalidKittyIndex);
     });
@@ -105,8 +105,8 @@ fn breed_failed_not_enough_balance_for_staking() {
         let account_id: u64 = 1;
         let other_account_id: u64 = 100;
         // parent kitty
-        let kitty_id_1 = 1u32;
-        let kitty_id_2 = 2u32;
+        let kitty_id_1 = 0u32;
+        let kitty_id_2 = 1u32;
         // 创建Kitty1
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id)));
         // 创建Kitty2
@@ -120,7 +120,7 @@ fn breed_failed_not_enough_balance_for_staking() {
 fn sell_works() {
     new_test_ext().execute_with(|| {
         let account_id: u64 = 1;
-        let kitty_id = 1u32;
+        let kitty_id = 0u32;
         let price = 100u128;
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id)));
         assert_ok!(SubstrateKitties::sell(Origin::signed(account_id), kitty_id, Some(price)));
@@ -136,7 +136,7 @@ fn sell_failed_not_owner() {
     new_test_ext().execute_with(|| {
         let account_id: u64 = 1;
         let other_account_id: u64 = 100;
-        let kitty_id = 1u32;
+        let kitty_id = 0u32;
         let price = 100u128;
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id)));
         assert_noop!(SubstrateKitties::sell(Origin::signed(other_account_id), kitty_id, Some(price)),Error::<Test>::NotOwner);
@@ -148,7 +148,7 @@ fn transfer_works() {
     new_test_ext().execute_with(|| {
         let account_id_1: u64 = 1;
         let account_id_2: u64 = 2;
-        let kitty_id = 1u32;
+        let kitty_id = 0u32;
         // 创建Kitty
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id_1)));
         // 转让Kitty
@@ -164,7 +164,7 @@ fn transfer_failed_not_owner() {
         let account_id_1: u64 = 1;
         let account_id_2: u64 = 2;
         let account_id_3: u64 = 3;
-        let kitty_id = 1u32;
+        let kitty_id = 0u32;
         // 创建Kitty
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id_1)));
         // 不是自己的Kitty
@@ -177,7 +177,7 @@ fn transfer_failed_not_enough_balance_for_staking() {
     new_test_ext().execute_with(|| {
         let account_id_1: u64 = 1;
         let account_id_5: u64 = 5;
-        let kitty_id = 1u32;
+        let kitty_id = 0u32;
         // 创建Kitty
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id_1)));
         // 余额不足无法质押
@@ -190,7 +190,7 @@ fn buy_works() {
     new_test_ext().execute_with(|| {
         let account_id_1: u64 = 1;
         let account_id_2: u64 = 2;
-        let kitty_id = 1u32;
+        let kitty_id = 0u32;
         let price: u128 = 1000;
         // 创建Kitty
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id_1)));
@@ -207,7 +207,7 @@ fn buy_works() {
 fn buy_failed_buyer_is_owner() {
     new_test_ext().execute_with(|| {
         let account_id_1: u64 = 1;
-        let kitty_id = 1u32;
+        let kitty_id = 0u32;
         let price: u128 = 1000;
         // 创建Kitty
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id_1)));
@@ -223,7 +223,7 @@ fn buy_failed_not_for_sell() {
     new_test_ext().execute_with(|| {
         let account_id_1: u64 = 1;
         let account_id_2: u64 = 2;
-        let kitty_id = 1u32;
+        let kitty_id = 0u32;
         // 创建Kitty
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id_1)));
         // Kitty未放入代售列表
@@ -236,7 +236,7 @@ fn buy_failed_buyer_not_enough_balance_for_buying() {
     new_test_ext().execute_with(|| {
         let account_id_1: u64 = 1;
         let account_id_3: u64 = 3;
-        let kitty_id = 1u32;
+        let kitty_id = 0u32;
         let price: u128 = 100000;
         // 创建Kitty
         assert_ok!(SubstrateKitties::create(Origin::signed(account_id_1)));
